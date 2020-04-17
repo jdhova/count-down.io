1b6826c10f4cf2c08bad3cc2dd4b3a945fa2b051
Jude Okagu <juud0010@gmail.com>
2020-04-17 02:35:19 +0200

    almost done

--- script.js
+++ script.js
@@ -3,39 +3,40 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
-
-
+const years = document.getElementById('years');
+const months = document.getElementById('months');
 
 const targetDate = new Date('June 20 2023 00:00:00');
 
-// const currentDate = new Date().getFullYear()
-
-const currentTime = new Date()
-
-const interval = targetDate - currentTime;
-
-const y = Math.floor(interval/1000/60/60/24/365);
-const m  = Math.floor(interval/1000/60/60/24);
-const d  = Math.floor(interval/1000/60/60);
-
-console.log(m,d)
-
-// console.log(d,targetDate,currentTime,'here')
 
 
 function millionTarget() {
-    const y = Math.floor(interval/1000/60/60/24/365);
-    const m  = Math.floor(interval/1000/60/60/24);
-    const d  = Math.floor(interval/1000/60/60);
 
-    console.log(m,d)
+const currentTime = new Date()
+const interval = targetDate - currentTime;
+
+const y = Math.floor(interval/1000/60/60/24/12/30);
+ const m = Math.floor(interval/1000/60/60/24/12) % 30
+ const d = Math.floor(interval/1000/60/60/24) % 12
+ const min = Math.floor( (interval/1000/60) % 60 );
+ const h = Math.floor( (interval/(1000*60*60)) % 24 );
+ const sec = Math.floor( (interval/1000) % 60 );
+
+ days.innerHTML = d
+ hours.innerHTML = h
+ minutes.innerHTML = min
+ seconds.innerHTML = sec
+ years.innerHTML = y
+ months.innerHTML = m
+
+
 }
 
 
 
 
 
-// setInterval();
+ setInterval(millionTarget,1000);
 
 
 
